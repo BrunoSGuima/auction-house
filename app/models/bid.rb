d4909f293334ab46183bcebdd80bc3d138de0b5c
Bruno Guimarães <105590450+BrunoSGuima@users.noreply.github.com>
2023-05-18 21:53:41 -0300

    update na Entrega do Projeto

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -15,7 +15,7 @@ class Bid < ApplicationRecord
   def greater_than_current_bid
     if auction_lot.bids.any?
       max_bid = auction_lot.bids.maximum(:amount) || 0
-      if amount <= max_bid + auction_lot.diff_min
+      if amount < max_bid + auction_lot.diff_min
         errors.add(:amount, "deve ser maior que o lance atual somado com a diferença mínima.")
       end
     end
